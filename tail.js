9ff882d4a876569480f609c9506634638ca0d825
Viktor <v.bolotayev@gmail.com>
2023-05-22 18:03:21 +0000

    Firstt commit tail.js

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertation Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+  
+  
+const tail = function(array) {
+  return array.slice(1);
+};
+  
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const test = ["test"];
+const empty = [];
+
+console.log(tail(words));
+console.log(tail(test));
+console.groupCollapsed(tail(empty));
+
+
+
